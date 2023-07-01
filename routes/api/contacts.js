d6b571239d611749298bb41c46033c934c24a456
Margaryta Myagkova <margomyagkova@gmail.com>
2023-07-01 13:32:52 +0300

    Updated

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,70 +1,26 @@
 const express = require("express");
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
-const { contactValidator } = require("../../utils/validator");
+const { schemas } = require("../../models/contacts");
+const ctrl = require("../../controllers/contacts");
+const { isValidId, validateBody } = require("../../utils/validator");
 
 const router = express.Router();
 
-const handleErrors = (res, status, message) =>
-  res.status(status).json({ message });
+router
+  .route("/")
+  .get(ctrl.listContacts)
+  .post(validateBody(schemas.addSchema), ctrl.addContact);
 
-router.get("/", async (req, res) => {
-  try {
-    res.json(await listContacts());
-  } catch (error) {
-    handleErrors(res, 500, "Internal server error");
-  }
-});
+router
+  .route("/:id")
+  .get(isValidId, ctrl.getContactById)
+  .delete(isValidId, ctrl.removeContact)
+  .put(isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
 
-router.get("/:contactId", async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    contact ? res.json(contact) : handleErrors(res, 404, "Not found");
-  } catch (error) {
-    handleErrors(res, 500, "Internal server error");
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const { error } = contactValidator(req.body);
-    if (error) return handleErrors(res, 400, error.details[0].message);
-    res.json(await addContact(req.body));
-  } catch (error) {
-    handleErrors(res, 500, "Internal server error");
-  }
-});
-
-router.delete("/:contactId", async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await removeContact(contactId);
-    contact
-      ? res.json({ message: "Contact deleted" })
-      : handleErrors(res, 404, "Not found");
-  } catch (error) {
-    handleErrors(res, 500, "Internal server error");
-  }
-});
-
-router.put("/:contactId", async (req, res) => {
-  try {
-    const { error } = contactValidator(req.body);
-    if (error) return handleErrors(res, 400, error.details[0].message);
-    const { contactId } = req.params;
-    const { name, email, phone } = req.body;
-    if (!name && !email && !phone)
-      return handleErrors(res, 400, "Missing fields");
-    res.json(await updateContact(contactId, req.body));
-  } catch (error) {
-    handleErrors(res, 500, "Internal server error");
-  }
-});
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
--- utils/validator.js
+++ utils/validator.js
@@ -1,11 +1,33 @@
-const joi = require("joi");
+const { isValidObjectId } = require("mongoose");
 
-const contactSchema = joi.object({
-  name: joi.string().min(3),
-  email: joi.string().email(),
-  phone: joi.string().min(5),
-});
+const httpError = (status, message) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
 
-const contactValidator = (body) => contactSchema.validate(body);
+const isValidId = (req, res, next) => {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    throw httpError(400, `${id} is not a valid ID`);
+  }
+  next();
+};
 
-module.exports = { contactValidator };
+const validateBody = (schema) => (req, res, next) => {
+  const { body } = req;
+  if (!body || Object.keys(body).length === 0) {
+    throw httpError(400, "Missing fields");
+  }
+
+  const { error } = schema.validate(body);
+  if (error) {
+    throw httpError(400, error.details[0].message);
+  }
+  next();
+};
+
+module.exports = {
+  isValidId,
+  validateBody,
+};
